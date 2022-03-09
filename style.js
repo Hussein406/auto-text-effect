dc3a09e5dae2258c0d3fa494a0e9daa79ef54409
Abdillahi Hussein <abdillahi.hussein@ibm.com>
2022-03-09 12:07:39 -0400

    Init empty branch

---
+++ style.js
@@ -0,0 +1,24 @@
+const textEl = document.querySelector("#text");
+const speedEl = document.querySelector("#speed");
+const text = "We love Programming";
+let index = 1;
+
+let speed = 300 / speedEl.value;
+
+const writeText = () => {
+  textEl.innerHTML = text.slice(0, index);
+
+  index++;
+
+  if (index > text.length) {
+    index = 1;
+  }
+
+  setTimeout(writeText, speed);
+};
+
+writeText();
+
+speedEl.addEventListener("input", (e) => {
+  speed = 300 / e.target.value;
+});
